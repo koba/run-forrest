5ec1281bd85caeed5b04827e1e675d6b58371455
Agustín Rodríguez <agurz@icloud.com>
2018-04-26 22:08:46 -0300

    fetch run and runState data from server

--- components/runDetail.js
+++ components/runDetail.js
@@ -7,58 +7,69 @@ import runsService from '../services/runs';
 
 import { darkTheme } from '../themes';
 
+const USER_ID = '5ae0e171677187162a948b8a'; // TEMP
+const RUN_ID = '5ae1fc2d861bb81bea245ec8'; // TEMP
+
 export default class RunDetail extends React.Component {
+
     constructor() {
         super();
+
         this.state = {
             Progress_Value: 0.00,
             Latitude: 0,
             Longitude: 0,
-            time: new Date()
+            run: null,
+            runState: null,
+            time: new Date(0, 0, 0, 0, 0, 0)
         }
 
-        var socket = Socket('http://agurz.ddns.net:52982');
+        this.loadRun(RUN_ID); // TODO: run should be passed as a prop
+        this.sendLocationPeriodically();
+        this.startTimer();
+    }
+
+    loadRun(id) {
+        runsService.getRun(id).then(res => {
+            this.setState({ run: res.data });
+            runsService.getRunState(id).then(res => {
+                this.setState({ runState: res.data });
+            })
+        });
+    }
+
+    sendLocationPeriodically() {
+        var socket = Socket('http://agurz.ddns.net:49442');
 
         setInterval(() => {
             navigator.geolocation.getCurrentPosition(
                 (position) => {
-                    this.setState({
+                    socket.emit('refresh', {
+                        user: '5ae1d52121da2b18b45d7435',
                         latitude: position.coords.latitude,
                         longitude: position.coords.longitude
                     });
-                    /*socket.emit('refresh', {
-                        user: '5ae1d52121da2b18b45d7435',
-                        latitude: this.state.latitude, // user lat
-                        longitude: this.state.latitude  // user lon
-                    });
-                    console.log(this.state.latitude);
-                    console.log(this.state.longitude);*/
                 },
             );
-        }, 500);
+        }, 10000);
 
         socket.on('refresh', (data) => {
-            console.log(data);
+            //this.setState({ runState: data });
         });
     }
 
-
-
-    Start_Progress = () => {
-        this.value = setInterval(() => {
-            if (this.state.Progress_Value <= 1) {
-                this.setState({ Progress_Value: this.state.Progress_Value + .01 })
-            }
-        }, 1500);
+    startTimer() {
+        setInterval(() => {
+            this.setState({ time: new Date(this.state.time.getFullYear(), this.state.time.getMonth(), this.state.time.getDay(), this.state.time.getHours(), this.state.time.getMinutes(), this.state.time.getSeconds() + 1) });
+        }, 1000);
     }
 
     render() {
-        this.Start_Progress();
         return (
             <ImageBackground source={require('../themes/assets/background.png')} style={darkTheme.imageBackground}>
                 <View style={{ paddingTop: 12 }}>
                     <Text style={{ ...darkTheme.text, textAlign: 'left', padding: 8 }}>Actualmente corriendo</Text>
-                    <Text style={{ ...darkTheme.title, backgroundColor: 'rgb(33, 90, 128)' }}>BOSTON</Text>
+                    <Text style={{ ...darkTheme.title, backgroundColor: 'rgb(33, 90, 128)' }}>{this.state.run && this.state.run.name.toUpperCase()}</Text>
                 </View>
                 <View>
                     <Text style={{ ...darkTheme.text }}>Posición</Text>
@@ -71,15 +82,27 @@ export default class RunDetail extends React.Component {
                     </View>
                     <View style={{ marginRight: 32 }}>
                         <Text style={{ ...darkTheme.text }}>km</Text>
-                        <Text style={{ ...darkTheme.textBold }}>{'2.47'}</Text>
+                        <Text style={{ ...darkTheme.textBold }}>
+                            {
+                                this.state.runState && 
+                                Object.keys(this.state.runState.runners).length && 
+                                Object.keys(this.state.runState.runners[USER_ID]).length && 
+                                (this.state.runState.runners[USER_ID].route.distance / 1000).toFixed(2)
+                            }
+                        </Text>
                     </View>
                     <View>
                         <Text style={{ ...darkTheme.text }}>paso</Text>
-                        <Text style={{ ...darkTheme.textBold }}>{'5 km/m'}</Text>
+                        <Text style={{ ...darkTheme.textBold }}>{'5"'}</Text>
                     </View>
                 </View>
                 <View>
-                    <Text style={{ ...darkTheme.text, textAlign: 'left' }}> Progress Value: {parseFloat((this.state.Progress_Value * 100).toFixed(3))} %</Text>
+                    {
+                        this.state.runState && 
+                        Object.keys(this.state.runState.runners).length && 
+                        Object.keys(this.state.runState.runners[USER_ID]).length && 
+                        <Text style={{ ...darkTheme.text, textAlign: 'left' }}> Progreso: {parseFloat((this.state.run.distance * 1000 * 100 / this.state.runState.runners[USER_ID].route.distance).toFixed(3))} %</Text>
+                    }
                     {
                         (Platform.OS === 'android')
                             ?
--- services/runs.js
+++ services/runs.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class runsService {
+    getRun(id) {
+        return axios.get('http://agurz.ddns.net:9000/runs/'+id+'?access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVhZTFkNTIxMjFkYTJiMThiNDVkNzQzNSIsImlhdCI6MTUyNDc2MDUyMH0.cM_kFZ9tarf2V0hoz6OG51529cU2KLoOtvP9Ox6iZPc')
+    }
+    getRunState(id) {
+        return axios.get('http://agurz.ddns.net:9000/runs/'+id+'/state?access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVhZTFkNTIxMjFkYTJiMThiNDVkNzQzNSIsImlhdCI6MTUyNDc2MDUyMH0.cM_kFZ9tarf2V0hoz6OG51529cU2KLoOtvP9Ox6iZPc')
+    }
     getRuns() {
         return axios.get('http://agurz.ddns.net:9000/runs?access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVhZTFkNTIxMjFkYTJiMThiNDVkNzQzNSIsImlhdCI6MTUyNDc2MDUyMH0.cM_kFZ9tarf2V0hoz6OG51529cU2KLoOtvP9Ox6iZPc')
-            //headers: {
-            //    'Content-Type': 'application/json',
-            //    'access_token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVhZTFkNTIxMjFkYTJiMThiNDVkNzQzNSIsImlhdCI6MTUyNDc2MDUyMH0.cM_kFZ9tarf2V0hoz6OG51529cU2KLoOtvP9Ox6iZPc'
-            //},
-            //data: JSON.stringify({ })
     }
 }
 
